8266ea9db0bbae3b93ad6f37e6f0214443cb4955
Itsca <itscae@gmail.com>
2018-09-30 22:23:39 -0600

    • Updates .yml to include alexa skill event and skill Id • Updates function names for lightBill function.

--- handler.js
+++ handler.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports.hello = async (event, context) => {
+module.exports.lightBill = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
