20b5ae0c266bc480898e5a9045288aab6a15a0e4
Itsca <itscae@gmail.com>
2018-10-08 23:06:01 -0600

    • Adds getLightBillsFromAPI, parseAPIResponsetoText, getResponse to return current bills based on NISE • Adds mock of current response in case we need it for testing

--- handler.js
+++ handler.js
@@ -1,19 +1,49 @@
 'use strict';
 
 module.exports.lightBill = async (event, context) => {
-  const response = {
-    version: '1.0',
-    statusCode: 200,
-    response: {
-      outputSpeech: {
-        type: 'PlainText',
-        text: `This is a test function.`,
+  let niseNumber = '27894731' // Hardcoded for testing, this one currently owes 3 months.
+  const getLightBillsFromAPI = function(nise) {
+    const baseUrl = 'https://agenciavirtual.cnfl.go.cr/cnfl/resources/proxy/pendientesAbierta/'
+    try {
+      APIResponse = fetch(baseUrl + nise)
+      return APIResponse
+    } catch(e) {
+      console.error(e)
+    }
+  }
+  const parseAPIResponsetoText = function() {
+    const apiResponse = getLightBillsFromAPI(niseNumber)
+    const totalBills = apiResponse.lenght
+    let textResponse
+    if (!apiResponse | totalBills <= 0) {
+      textResponse = 'You are all set!, no bills to pay.'
+      return textResponse
+    }
+    let finalResponse = 'You have a total of ' + totalBills + ' bills for C N F L'
+    apiResponse.forEach( bill => {
+      let {mescobro, fechavencimiento, saldocapital} = bill
+      textResponse = ' for the month of TEST for a total of ' +
+                    saldocapital + ' costa rican colon due for ' + 'TEST,'
+      finalResponse = finalResponse + textResponse
+    });
+    return finalResponse
+  }
+  const getResponse = function() {
+    const textResponse = parseAPIResponsetoText()
+    const response = {
+      version: '1.0',
+      statusCode: 200,
+      response: {
+        outputSpeech: {
+          type: 'PlainText',
+          text: textResponse,
+        },
+        shouldEndSession: false,
       },
-      shouldEndSession: false,
-    },
-  };
-  return response;
-
+    }
+    return response
+  }
+  return getResponse();
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
